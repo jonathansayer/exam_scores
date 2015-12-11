f298b056c07e0ba6212566b010905d905a33e346
jonathansayer <jon.sayer1@gmail.com>
2015-12-11 15:07:05 +0000

    added a function to calculate total of list

--- exam_scores.py
+++ exam_scores.py
@@ -1,8 +1,14 @@
 grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
-
+total = 0
 
 def print_grades(grades):
     for g in grades:
         print g
 
-print_grades(grades)
+def grades_sum(grades):
+    global total
+    for g in grades:
+        total += g
+
+grades_sum(grades)
+print total
