03a047d3a155396ab839e4bf7166a0eba0e9ff4b
jonathansayer <jon.sayer1@gmail.com>
2015-12-11 15:11:20 +0000

    added function to calculate average of grades

--- exam_scores.py
+++ exam_scores.py
@@ -1,14 +1,17 @@
 grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
-total = 0
+
 
 def print_grades(grades):
     for g in grades:
         print g
 
 def grades_sum(grades):
-    global total
+    total = 0
     for g in grades:
         total += g
+    return total
 
-grades_sum(grades)
-print total
+def grades_average(grades):
+    return grades_sum(grades)/float(len(grades))
+
+print grades_average(grades)
